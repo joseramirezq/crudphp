877fcdfd57eceef6c8479047e65b980347bc86b2
joseluis <joseramirezq.info@gmail.com>
2019-01-06 17:52:59 -0500

    Crear y eliminar task

---
+++ delete_task.php
@@ -0,0 +1,17 @@
+<?php
+include("db.php");
+
+if(isset($_GET['id'])){
+    $id=$_GET['id'];
+    $query="DELETE FROM task WHERE id=$id";
+    $result=mysqli_query($conn,$query);
+
+    if(!$result){
+        die("Query Fails");
+    }
+    $_SESSION['message']='Tarea eliminada satisfactoriamente';
+    $_SESSION['message_type']='danger';
+
+    header("Location:index.php");
+}
+?>
--- save_task.php
+++ save_task.php
@@ -1,6 +1,19 @@
 <?php
+    include("db.php");
+
     if(isset($_POST['save_task'])){
-        echo "saving";
+        $name=$_POST['title'];
+        $description=$_POST['description'];
+        
+        $query="INSERT INTO task(title, descripcion) VALUES ('$name','$description')";
+        $result=mysqli_query($conn,$query);
+
+        if(!$result){
+            die("Query Faild");
+         }
+         $_SESSION['message']='Tarea guardada exitosamente';
+         $_SESSION['message_type']='success';
+         
+        header("Location: index.php");
     }
 ?>
-    
